bbe4d1d72b4af2affe782b4301b12db41dd8b18b
Alexandru-Mircea Olteanu <alexandru.olteanu@tremend.com>
2019-12-30 15:53:17 +0200

    Script for publishing new version

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
-import os
 import glob
+import os
+import shutil
+import sys
 from setuptools import setup, Extension
 from setuptools.command.build_py import build_py as _build_py
+
+VERSION = '0.1.5'
+
+
+def read(f):
+    return open(f, 'r', encoding='utf-8').read()
 
 
 class build_ext_first(_build_py):
@@ -17,6 +25,7 @@ class build_ext_first(_build_py):
         _build_py.run(self)
         self.move_extension_to_package()
 
+
 nabto_api = Extension(
     '_nabto_api',
     sources=[
@@ -29,14 +38,33 @@ nabto_api = Extension(
     libraries=['nabto_client_api_static', 'nabto_static_external'],
 )
 
+if sys.argv[-1] == 'publish':
+    if os.system("pip freeze | grep twine"):
+        print("twine not installed.\nUse `pip install twine`.\nExiting.")
+        sys.exit()
+    os.system("python setup.py sdist")
+    if os.system("twine check dist/*"):
+        print("twine check failed. Packages might be outdated.")
+        sys.exit()
+    os.system("twine upload -r testpypi dist/*")
+    # print("You probably want to also tag the version now:")
+    # print("  git tag -a %s -m 'version %s'" % (VERSION, VERSION))
+    # print("  git push --tags")
+    shutil.rmtree('dist')
+    shutil.rmtree('nabto_client.egg-info')
+    sys.exit()
+
 setup(
     name='nabto_client',
-    version='0.1.0',
+    version=VERSION,
     cmdclass={'build_py': build_ext_first},
     packages=['nabto_client'],
     author='Alexandru Gandrabur',
     author_email='alexandru.gandrabur@tremend.com',
     description="""Nabto Client Wrapper for Python""",
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    python_requires=">=3.6",
     ext_modules=[nabto_api],
     classifiers=[
         'Programming Language :: Python :: 3.6',
