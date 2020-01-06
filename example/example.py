8f82997c30df7d1214e1eb0e02aa1a9e4b90a4f6
Alexandru Gandrabur <alexandru.gandrabur@tremend.com>
2020-01-06 11:29:48 +0200

    Improve example to work on localhost

--- example/example.py
+++ example/example.py
@@ -7,12 +7,15 @@ import nabto_client
 @profile
 def nabto_stuff():
     nabto_client.startup("/home/alex/work/nabto/python-nabto-client/example/share/nabto")
-    nabto_client.NabtoProfile.createSelfSignedProfile("alex", "mypassword")
+    # nabto_client.NabtoProfile.createSelfSignedProfile("alex", "mypassword")
 
-    with nabto_client.NabtoSession("alex6", "mypassword6") as session, \
-            nabto_client.NabtoTunnel(session, 0, 'sdktnens.ru4bi.appmyproduct.com', 'localhost', 6001) as port:
-        print(f'Opened tunnel on port {port}')
-        sleep(3)
+    with nabto_client.NabtoSession("alex", "mypassword") as session:
+        with open("./unabto_queries.xml") as file:
+            session.RpcSetDefaultInterface(file.read())
+            session.RpcInvoke("nabto://jkkecnxk.rxxbkt.trial.nabto.net/pair_with_device.json?name=alex")
+        with nabto_client.NabtoTunnel(session, 0, 'jkkecnxk.rxxbkt.trial.nabto.net', 'localhost', 8090) as port:
+            print(f'Opened tunnel on port {port}')
+            sleep(30)
     nabto_client.shutdown()
 
 nabto_stuff()
